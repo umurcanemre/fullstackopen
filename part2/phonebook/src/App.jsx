cb09fa6f21396c8ad1544631cb73ef4ff23f3f7f
umurcan <5785630+umurcanemre@users.noreply.github.com>
2026-08-24 17:22:55 +0200

    part2c phonebook completed 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Numbers from './components/Numbers'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '1205835' }
-  ])
+  const [persons, setPersons] = useState([])
   const [nameFilter, setNameFilter] = useState('')
 
+  useEffect(() => {    
+    console.log('effect')
+    axios.get('http://localhost:3001/persons')
+    .then(resp=> {
+      setPersons(resp.data)
+    })
+  }, [])
 
   const addPerson = (newName, newPhone) => {
     if (persons.some(p => p.name === newName)) {
