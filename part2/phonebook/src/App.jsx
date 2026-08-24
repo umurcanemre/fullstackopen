55ce394008b7800bd7c6348799cf2a3dd66b0de3
umurcan <5785630+umurcanemre@users.noreply.github.com>
2026-08-24 16:09:14 +0200

    refactors on part2 phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -7,44 +7,19 @@ const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '1205835' }
   ])
-  const [newName, setNewName] = useState('')
-  const [newPhone, setNewPhone] = useState('')
   const [nameFilter, setNameFilter] = useState('')
 
-  function phoneValid(no) {
-    if (no.charAt(0) === '+') {
-      return no.substring(1, no.length).split('').filter(c => c !== '-').every(char => char >= '0' && char <= '9')
-    }
-    else {
-      return no.split('').filter(c => c !== '-').every(char => char >= '0' && char <= '9')
-    }
-  }
 
-  const addPerson = (event) => {
-    event.preventDefault()
+  const addPerson = (newName, newPhone) => {
     if (persons.some(p => p.name === newName)) {
       alert(`${newName} is already on the phonebook`)
-    }
-    if (!phoneValid(newPhone)) {
-      alert(`${newPhone} is invalid`)
     }
     else {
       console.log("adding ", newName, newPhone)
       setPersons(persons.concat({ name: newName, number: newPhone }))
-      setNewName('')
-      setNewPhone('')
     }
   }
 
-  const controlName = (event) => {
-    console.log("entered ", event.target.value)
-    setNewName(event.target.value)
-  }
-  const controlPhone = (event) => {
-    const validity = phoneValid(event.target.value) ? 'valid' : 'invalid'
-    console.log(`entered ${validity} phone `, event.target.value)
-    setNewPhone(event.target.value)
-  }
   const filterName = (event) => {
     setNameFilter(event.target.value.toLowerCase())
   }
@@ -53,7 +28,7 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <Filter nameFilter={nameFilter} filterByName={filterName} />
-      <PersonForm addPerson={addPerson} newName={newName} controlName={controlName} newPhone={newPhone} controlPhone={controlPhone} />
+      <PersonForm addPerson={addPerson} />
       <Numbers persons={persons} nameFilter={nameFilter} />
     </div>
   )
--- part2/phonebook/src/components/PersonForm.jsx
+++ part2/phonebook/src/components/PersonForm.jsx
@@ -1,5 +1,36 @@
-const PersonForm = ({ addPerson, newName, controlName, newPhone, controlPhone }) => {
+import { useState } from "react"
 
+function phoneValid(no) {
+    if (no.charAt(0) === '+') {
+        return no.substring(1, no.length).split('').filter(c => c !== '-').every(char => char >= '0' && char <= '9')
+    }
+    else {
+        return no.split('').filter(c => c !== '-').every(char => char >= '0' && char <= '9')
+    }
+}
+
+const PersonForm = ({ addPerson }) => {
+    const [newName, setNewName] = useState('')
+    const [newPhone, setNewPhone] = useState('')
+
+    const controlName = (event) => {
+        console.log("entered ", event.target.value)
+        setNewName(event.target.value)
+    }
+    const controlPhone = (event) => {
+        const validity = phoneValid(event.target.value) ? 'valid' : 'invalid'
+        console.log(`entered ${validity} phone `, event.target.value)
+        setNewPhone(event.target.value)
+    }
+    const onSubmit = (event) => {
+        event.preventDefault()
+        if (!phoneValid(newPhone)) {
+            alert(`${newPhone} is invalid`)
+        }
+        addPerson(newName, newPhone)
+        setNewName('')
+        setNewPhone('')
+    }
     return (
         <>
             <h2>Add new entry</h2>
@@ -11,7 +42,7 @@ const PersonForm = ({ addPerson, newName, controlName, newPhone, controlPhone })
                     number: <input value={newPhone} onChange={controlPhone} />
                 </div>
                 <div>
-                    <button type="submit" onClick={addPerson}>add</button>
+                    <button type="submit" onClick={onSubmit}>add</button>
                 </div>
             </form>
         </>
