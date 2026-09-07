0a31325639005b7cb9de27ecb2836e251feb6977
umurcan <5785630+umurcanemre@users.noreply.github.com>
2026-09-08 00:32:32 +0200

    part4 blog added exec4.1

---
+++ part4/blog/app.js
@@ -0,0 +1,20 @@
+const blogRoute = require('./controllers/blog')
+const mongoose = require('mongoose')
+const express = require('express')
+const config = require('./utils/config')
+const log = require('./utils/logger')
+const middleware = require('./utils/middleware')
+
+const app = express()
+mongoose.connect(config.MONGODB_URI, { family: 4 })
+  .then(log.info('Mongo connection established'))
+  .catch(e => log.error('Mongo connection error: ', e))
+
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/blogs', blogRoute)
+app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint)
+
+
+module.exports = app
